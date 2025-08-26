beb9325d2c7c9d1adda666bedb7c8a40d8a70a36
dhineshneo <dhinesjkumar@neophrontech.com>
2025-08-26 12:41:46 +0530

    Correction commit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -44,7 +44,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "47b6860fda5385eb09012b3f49e50e64
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "5215c13c29f5b7afb1396dd642b53e76",
+"flutter_bootstrap.js": "7153ab0c071f1fc7c3fe0635b3bb1020",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
